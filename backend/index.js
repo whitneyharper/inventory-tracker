7ed4a6d74cc4df199073907db51139977a6565ec
Whitney Harper <whitneyharper1@aol.com>
2022-05-22 12:32:35 -0400

    changed port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 const cors = require("cors");
 
 //==================================================
