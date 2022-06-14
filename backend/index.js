fb1734ed3c3685851eeb3598167a7872c9767025
unknown <whitneyharper1@aol.com>
2022-06-14 16:55:04 -0400

    uncommented index.js code.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-// const path = require('path');
+const path = require('path');
 const mongoose = require('mongoose');
 const connectionString = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 3001;
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 //==================================================
 // Routes
