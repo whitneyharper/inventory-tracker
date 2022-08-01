b473360535f870397838a7bdcb40e1fe9da38526
unknown <whitneyharper1@aol.com>
2022-08-01 18:13:46 -0400

    Installed bcrypt. Removed nodemon as a dependency. Added logic to signup controller, along with hashing the password.

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 
 const asyncHandler = (cb) => {
@@ -19,5 +20,15 @@ exports.loginUser = asyncHandler(async(req, res) => {
 
 //SIGNUP USER
 exports.signupUser = asyncHandler(async(req, res) => {
-    
+    const exists = await User.findOne({ email: req.body.email });
+
+    if(exists){
+        res.status(409).json({message: "Email already in use"})
+    } 
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    const user = await User.create({email: req.body.email, password: hash});
+    return res.status(200).json({message: 'New user created', user});
 });
